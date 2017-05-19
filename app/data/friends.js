72552bee610eba2480f4a896949dc7a2f102a013
Chris Aja <owner2@anas-mbp.gateway.pace.com>
2017-05-18 23:10:44 -0700

    hw 11

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [ {
+	"name" : "Tina",
+	"photo" : "https://vignette2.wikia.nocookie.net/p__/images/1/18/Tina_Belcher.jpg/revision/latest?cb=20150211193049&path-prefix=protagonist",
+	"scores" : [5,5,5,3,3,5,5,5,5,5]
+}, {
+	"name" : "Gene",
+	"photo" : "https://vignette2.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Gene_Belcher.png/revision/latest?cb=20130114043308",
+	"scores" : [3,3,3,3,5,3,4,1,5,3]
+}, {
+	"name" : "Louise",
+	"photo" : "https://vignette2.wikia.nocookie.net/bobsburgerpedia/images/b/b9/Louise_render.png/revision/latest?cb=20130113075349",
+	"scores" : [1,1,2,5,1,1,1,1,3,2]
+// }, {
+// 	"name" : "Linda",
+// 	"photo" : "https://vignette2.wikia.nocookie.net/bobsburgerpedia/images/3/33/Linda_belcher.jpg/revision/latest?cb=20121117161722",
+// 	"scores" : [5,5,5,4,3,1,5,1,5,5]
+// }, {
+// 	"name" : "Bob",
+// 	"photo" : "https://static.tvtropes.org/pmwiki/pub/images/bsbs_Bob_2F_8432.jpg",
+// 	"scores" : [2,5,1,5,1,1,3,1,4,5]
+}];
+
+
+module.exports = friends;
