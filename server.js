95c6d704ec38412b8ee3a9c3aa47d9467a45a4cc
Chris Aja <owner2@Anas-MacBook-Pro.local>
2017-05-23 13:23:33 -0700

    fixing issues

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
+
 var PORT = process.env.PORT || 3000;
 
+app.use(express.static(process.cwd() + "/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
